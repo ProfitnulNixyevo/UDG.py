37bd61908f63c111373f98a3f616fd1d6fc92c31
ProfitnulNixyevo <149631286+ProfitnulNixyevo@users.noreply.github.com>
2023-11-01 23:17:33 +0100

    Add files via upload

---
+++ Lesson_1.py
@@ -0,0 +1,40 @@
+import math
+
+# 1. Zadatak
+# a = int(input("a ="))
+# b = int(input("b ="))
+
+# povrsina = a * b
+
+# print(f"{povrsina} sm")
+
+# 2. Zadatak
+
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+
+# d = pow(b,2) - 4 * a * c
+# x1 = ((-b) + (math.sqrt(d))) / 2 * a
+# x2 = ((-b) - (math.sqrt(d))) / 2 * a
+
+# print(f"x1 = {x1}, x2 = {x2}")
+
+# 3. Zadatak
+
+# a = int(input("a = "))
+# b = int(input("b = "))
+
+# c = math.pow(a, 2) - math.pow(b, 2)
+# d = (a - b) * (a + b)
+
+# print(f"1 (c) \n 2 {d}")
+
+# 4. Zadatak
+
+# d = int(input("d = "))
+# s = int(input("s = "))
+
+# x = ((d + s) * 2) * 4
+
+# print(x)
