0d4a4405fe285d495c9b27269f8bcd414fcca11d
UnderDog <149631286+ProfitnulNixyevo@users.noreply.github.com>
2023-11-03 11:04:43 +0100

    Add files via upload

---
+++ Classwork_2.py
@@ -0,0 +1,56 @@
+import math
+
+# domahi zadatak 1
+
+# a = int(input("Введите ширину листа бумаги: "))
+# b = int(input("Введите  высоту листа бумаги: "))
+
+# povrsina = (a * b)/10
+# povrsinacm = (2*(a + b))/100
+
+
+# print(f" {povrsina} cм^2 \n {povrsinacm} cm ")
+
+# domahi zadatak 2
+
+
+# a = int(input("а = : "))
+# b = int(input("b = : "))
+# c = int(input("c = : "))
+
+# result = math.pow(a,2) + math.pow(b,2) + math.pow(c,2) + (2 * a * b) + (2 * a * c) + (2 * b * c)
+
+# print (f"Result : {result}")
+
+#
+# h = float(input("insert hours:  "))
+
+# liter = h * 0.5
+# liter_o = int(liter)
+
+# print(f"He drink {liter_o} liters at {h} hours")
+
+# zadatak 8
+
+# P =  float(input("insert P :  "))
+
+# pi = 3.14
+
+# radius = math.sqrt( P/ pi) # find radius
+
+# result = 2 * pi * radius  # find p
+
+# print(f'Result : {result}')
+
+
+# zadatak 9
+d = int(input("d = : "))
+s = int(input("s = : "))
+r = int(input("s = : "))
+
+d1 = d + (2 * r)
+s1 = s + (2 * r)
+
+result = 2 * (d1 + s1)
+
+print(f"Result :  {result} - duzina terena u metrima")
